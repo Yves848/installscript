27019e0fdcc6c2b74ea59d69f9767583a33c86cb
yves848 <yves.godart@gmail.com>
2022-08-16 20:35:40 +0200

    second

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 # Environment Variables
 [System.Environment]::SetEnvironmentVariable('DOTNET_CLI_TELEMETRY_OPTOUT', '1', [EnvironmentVariableTarget]::Machine)
 
-# Windows Features
+# Windows Features 
 # List features: Get-WindowsOptionalFeature -Online
 Enable-WindowsOptionalFeature -Online -FeatureName 'Containers' -All
 Enable-WindowsOptionalFeature -Online -FeatureName 'Microsoft-Hyper-V' -All
